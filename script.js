df8ae0c066a31adae1207e99284522daf2fb4ff6
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-11-21 11:15:09 +0530

    switch statements

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Nested If-Else
 
 // Example
-let year = 2002;
+/* let year = 2002;
 let IsleapYear;
 
 if(year %4 ===0){
@@ -20,4 +20,36 @@ if(year %4 ===0){
     IsleapYear = false;
 }
 
-console.log(IsleapYear ? "Leap Year" : "Not A Leap Year");
+console.log(IsleapYear ? "Leap Year" : "Not A Leap Year"); */
+
+//switch statements
+
+let month = parseInt (prompt("Enter a month to find the number of days in that month"));
+
+switch(month){
+    case 1 : console.log(31);
+    break;
+    case 2 : console.log(28);
+    break;
+    case 3 : console.log(31);
+    break;
+    case 4 : console.log(30);
+    break;
+    case 5 : console.log(31);
+    break;
+    case 6 : console.log(30);
+    break;
+    case 7 : console.log(31);
+    break;
+    case 8 : console.log(31);
+    break;
+    case 9 : console.log(30);
+    break;
+    case 10 : console.log(31);
+    break;
+    case 11 : console.log(30);
+    break;
+    case 12 : console.log(31);
+    break;
+    default : console.log("Enter a number between 1 to 12");
+}
